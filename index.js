852630ce87c8a54119dbedbf812492d5c279bc85
Zach Brookman <zachbrookman48@gmail.com>
2021-05-06 09:48:06 -0400

    commit tree 6075ac95a9b07ae0726747657f4921e0a6edb4c3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, matchingDrivers){
+    return drivers.filter(function (matches) {
+    return matches.toLowerCase() === matchingDrivers.toLowerCase()})
+    }
+//this is that same function as an arrow function
+//  function findMatching(drivers, matchingDrivers){
+//  return drivers.filter(matches => matches.toLowerCase() === matchingDrivers.toLowerCase())
+//  }
+  function fuzzyMatch(drivers, matchingDriversLetters){
+ return drivers.filter(element => element.toLowerCase().indexOf(matchingDriversLetters.toLowerCase())=== 0)
+  }
+ function matchName(drivers, driverName){
+return drivers.filter( driverMatch => driverMatch.name === driverName)
+ }
